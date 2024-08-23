50ba05a67671eb4b417b052cc477e2bf9c6486c6
Padma1501 <padma.kuppela1501@outlook.com>
2024-08-23 15:18:01 +0530

    Lab003 added

---
+++ src/ex_08082024/Lab001.py
@@ -0,0 +1,6 @@
+# This is comment-which doesnt run
+print("Hello padma, Welcome to the learning python")
+print(2+2)
+print(2-2)
+
+
---
+++ src/ex_10082024/Lab003.py
@@ -0,0 +1,26 @@
+# python
+# Installed the pycharm-pycharm community edition
+# https://www.jetbrains.com/pycharm/download/
+
+# community edition is FREE and open source
+# Git is installed-git.exe is not found- https://git-scm.com/downloads
+# Restart your system
+print("Hello world!")
+
+# Builtsin.py all the important already bult by python
+# These are like utilities.
+
+#   def print (self, *args, sep=' '. end='/n' file=None):
+# print()
+# self- concept in OOPs which points to itself-Ignore
+# *args- Unlimited no. of arguements *-String, float, boolean
+# file=None-file 10
+
+
+# sep=' '-How you want to seperate the arguements
+# end='/n'- In the end what you want to do.
+
+print("Hello", "world", "Padma", 123, 9.5, True, sep="-")  # *args
+print("Hello", "world", "Padma", 123, 9.5, True, sep="/", end="_")  # end
+print("padma")
+print("Hello", "world", "Padma", 123, 9.5, True, sep="/", end="  ")  # end
