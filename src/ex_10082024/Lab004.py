2def2f16ded72bcdcd02fb1ad876319ccd9774c5
Padma1501 <padma.kuppela1501@outlook.com>
2024-08-24 21:18:16 +0530

    Lab004 added

--- src/ex_10082024/Lab004.py
+++ src/ex_10082024/Lab004.py
@@ -1,2 +1,5 @@
-  print("Hello world")
-#
+#  print("Hello world")
+# select the file and reformate the code
+# Print("Hello padma")
+# Above code is not run bcoz P is capital ( Python is case sensitive language )i.e; print is not run
+print("Welcome to the code")
